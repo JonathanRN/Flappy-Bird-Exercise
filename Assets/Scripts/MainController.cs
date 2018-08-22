b122dec34cffb1ec07166b605dc10941cbb488b2
JonathanRN <minijona7@gmail.com>
2018-08-22 14:01:03 -0400

    Configured the project

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+	private static bool IsSceneLoaded(string name)
+	{
+		for (int i = 0; i < SceneManager.sceneCount; i++)
+		{
+			if (SceneManager.GetSceneAt(i).name == name)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void LoadGame()
+	{
+		if (!IsSceneLoaded(Scenes.Game))
+		{
+			SceneManager.LoadSceneAsync(Scenes.Game, LoadSceneMode.Additive);
+		}
+
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName(Scenes.Game));
+	}
+
+	// Use this for initialization
+	void Start () {
+		LoadGame();
+	}
+}
+
+public static class Scenes
+{
+	public const string Main = "Main";
+	public const string Game = "Game";
+}
---
+++ Assets/Scripts/Playmode/Player/TranslateMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateMover : MonoBehaviour {
+
+	[SerializeField] private int speed = 5;
+
+	public void Move(Vector3 direction)
+	{
+		transform.Translate(direction * speed * Time.deltaTime);
+	}
+}
